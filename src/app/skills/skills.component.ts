707e463c7d6244bd754ea30cfa313f201c922d4e
Héctor Miguel <127361651+SoyEtoor@users.noreply.github.com>
2024-09-26 16:46:19 -0600

    TO mycv (#40)
    * add skills branch Closes #4 (#21)
    
    * create develop branch
    
    * create develop
    
    * create header branch #1
    
    * create header branch #1
    
    * add karma.conf
    
    * add work experience branch
    
    * add education branch #3
    
    * add skills branch #4
    
    * add karma.conf
    
    * Create master_CI_CD.yml (#37)
    
    * create release branch (#9)
    
    * create release branch
    
    * create release
    
    * add karma.conf
    
    * Soy etoor patch 4 (#34)
    
    * add skills branch Closes #4 (#21)
    
    * create develop branch
    
    * create develop
    
    * create header branch #1
    
    * create header branch #1
    
    * add karma.conf
    
    * add work experience branch
    
    * add education branch #3
    
    * add skills branch #4
    
    * Create release_CI_DFD.yml
    
    * Update and rename release_CI_DFD.yml to release_CI_CD.yml (#35)
    
    * Update readme.md (#36)
    
    * Develop (#38)
    
    * create develop branch
    
    * create develop
    
    * create header branch #1
    
    * create header branch #1
    
    * add karma.conf
    
    * Create main.yml
    
    * Revert "Create main.yml"
    
    * Create develop_CI_CD.yaml
    
    * Update develop_CI_CD.yaml
    
    * add work experience branch
    
    * add education branch #3
    
    * Update develop_CI_CD.yaml (#18)
    
    * Update develop_CI_CD.yaml (#19)
    
    * Update develop_CI_CD.yaml
    
    * Update develop_CI_CD.yaml
    
    * Update develop_CI_CD.yaml
    
    * Update develop_CI_CD.yaml
    
    * Edit education part (#20)
    
    * Revert 21 skills (#22)
    
    * add skills branch Closes #4 (#21)
    
    * create develop branch
    
    * create develop
    
    * create header branch #1
    
    * create header branch #1
    
    * add karma.conf
    
    * add work experience branch
    
    * add education branch #3
    
    * add skills branch #4
    
    * Revert "add skills branch Closes #4 (#21)"
    
    This reverts commit b298531d66bfeee45a7d751c0ba9966d33612a90.
    
    * add skills branch #4 (#23)
    
    * add interest branch #6 (#26)
    
    * add achievemts branch #7 (#27)
    
    * add css to header branch #1 (#28)
    
    * add css to work branch #2 (#29)
    
    * Edit education part (#30)
    
    * add skills branch #4 (#31)
    
    * add interest branch #6 (#32)
    
    * add achievemts branch #7 (#33)
    
    * Update app.component.ts (#39)

---
+++ src/app/skills/skills.component.ts
@@ -0,0 +1,53 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  selector: 'app-skills',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './skills.component.html',
+  styleUrl: './skills.component.css'
+})
+
+export class SkillsComponent {
+  skills: Array<any> = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    let skills1 = {
+      skill: "Leadership"
+    };
+
+    let skills2 = {
+      skill: "Communication"
+    };
+
+    let skills3 = {
+      skill: "Problem-Solving"
+    };
+
+    let skills4 = {
+      skill: "Web Desing"
+    };
+
+    let skills5 = {
+      skill: "Database management"
+    };
+
+    let skills6 = {
+      skill: "Data analysis"
+    };
+
+
+    this.skills.push(skills1);
+    this.skills.push(skills2);
+    this.skills.push(skills3);
+    this.skills.push(skills4);
+    this.skills.push(skills5);
+    this.skills.push(skills6);
+
+    console.log(this.skills);
+  }
+}
+
